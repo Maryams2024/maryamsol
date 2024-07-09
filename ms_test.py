23d5ceb7c07f7f74856f45e652581f1626a54919
maryam soltanpour <maryam.soltanpour.hmhn.org>
2024-07-08 22:53:54 -0400

    another function added

--- ms_test.py
+++ ms_test.py
@@ -10,3 +10,31 @@ def fibonacci(n):
 num_terms = 10  
 fib_series = fibonacci(num_terms)
 print(fib_series)
+
+
+
+def is_prime(num):
+    """Check if a number is prime."""
+    if num <= 1:
+        return False
+    if num == 2:
+        return True  # 2 is the smallest prime number
+    if num % 2 == 0:
+        return False  # Even numbers greater than 2 are not prime
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def smallest_10_primes():
+    """Find the smallest 10 prime numbers."""
+    primes = []
+    num = 2  # smallest prime number
+    while len(primes) < 10:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+prime_numbers = smallest_10_primes()
+print("The smallest 10 prime numbers are:", prime_numbers)
